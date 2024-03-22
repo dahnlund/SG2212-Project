048067a25d1667f4f93be6745768bdf7a1616381
dahnlund <david.ahnlund@gmail.com>
2024-03-22 18:27:30 +0100

    Uploaded probe analysis file

--- python/probe.py
+++ python/probe.py
@@ -1,15 +1,15 @@
+#%%
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
 
-mat = loadmat('diag_data.mat')
+#mat = loadmat('diag_data.mat')
+mat = loadmat("/Users/david/KTH/SG2212-Project/diag_data.mat")
 t = mat['t'][0]
 uvel = mat['uvel']
 cases = [25, 250, 5000]
 
-duvel = np.diff(uvel, axis = 0)/np.diff(t)[0]
-
-print(np.median(uvel,axis = 0))
+d_uvel = np.diff(uvel, axis = 0)/np.diff(t)[0]
 
 plt.figure()
 plt.plot(t, uvel)
@@ -21,17 +21,10 @@ plt.grid()
 plt.savefig("./plots/plot1.png", bbox_inches='tight')
 
 
-rel_deriv = duvel/np.median(uvel,axis = 0)
-plt.figure()
-plt.plot(t[1:], rel_deriv)
-plt.ylabel("U")
-plt.xlabel("time")
-plt.legend(cases)
-plt.grid()
-plt.savefig("./plots/plot1derivative.png", bbox_inches='tight')
+rel_deriv = d_uvel/np.abs(np.median(uvel,axis = 0))
 
-
-
-print(np.abs(np.argmax(np.abs(rel_deriv[:,1]-np.median(uvel,axis = 0)[1])<0.01)))
 for col in range(uvel.shape[1]):
-    print(t[np.argmax(np.abs(rel_deriv[:,col]) < 0.01)])
+    cond1 = np.abs(uvel[1:,col]-np.median(uvel,axis = 0)[col]) < \
+        0.05 * np.abs(np.median(uvel,axis = 0)[col])
+    cond2 = np.abs(rel_deriv[:,col]) < 0.05
+    print(f'Time to steady flow when Re = {cases[col]}: {t[np.argmax(cond1*cond2)]}')
